f0a9c52cd5413fcb3ab07a1e8ee72ed38e4c9366
aloveland <ajlovelandwater@gmail.com>
2024-12-06 15:35:58 -0500

    3371

--- 3371_Identify_the_Largest_Outlier_in_an_Array.py
+++ 3371_Identify_the_Largest_Outlier_in_an_Array.py
@@ -1,9 +1,6 @@
-"""3371. Identify the Largest Outlier in an Array
-Solved
+"""
+3371. Identify the Largest Outlier in an Array
 Medium
-Topics
-Companies
-Hint
 You are given an integer array nums. This array contains n elements, where exactly n - 2 elements are special numbers. One of the remaining two elements is the sum of these special numbers, and the other is an outlier.
 
 An outlier is defined as a number that is neither one of the original special numbers nor the element representing the sum of those numbers.
